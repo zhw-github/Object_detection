51c5ec1e427d6913233926d8da489257c9381c1c
zhw-github <zhw859719854@163.com>
2021-12-19 14:12:34 +0800

    edit utils firstly

---
+++ Faster-RCNN/utils/utils.py
@@ -0,0 +1,22 @@
+import numpy as np
+from PIL import Image
+
+
+#---------------------------------------------------------#
+#   将图像转换成RGB图像，防止灰度图在预测时报错。
+#   代码仅仅支持RGB图像的预测，所有其它类型的图像都会转化成RGB
+#---------------------------------------------------------#
+def cvtColor(image):
+    if len(np.shape(image)) == 3 and np.shape(image)[2] == 3:
+        return image 
+    else:
+        image = image.convert('RGB')
+        return image 
+
+#---------------------------------------------------#
+#   对输入图像进行resize
+#---------------------------------------------------#
+def resize_image(image, size):
+    w, h        = size
+    new_image   = image.resize((w, h), Image.BICUBIC)
+    return new_image
